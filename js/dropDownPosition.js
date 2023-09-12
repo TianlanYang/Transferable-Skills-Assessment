ccc87a4f0f7c180f100e0342b76a9a692fa6c830
Tianlan Yang <yangtianlan@ufl.edu>
2023-09-12 14:48:51 -0400

    Diya's Change

--- js/dropDownPosition.js
+++ js/dropDownPosition.js
@@ -3,31 +3,28 @@
 
 // define an object
 var sportPositions = {
-    'Badminton': ['Singles', 'Doubles Forecourt', 'Doubles Backcourt'],
-    'Baseball/softball': ['Pitcher', 'Catcher', 'First baseman', 'Second baseman','Third baseman','Shortstop','Left fielder','Center fielder','Right fielder'],
-    'Basketball': ['Point Guard', ,'Shooting Guard','Small Forward', 'Power Forward','Center'],
+    'Badminton': ['','Singles', 'Doubles Forecourt', 'Doubles Backcourt'],
+    'Baseball/softball': ['','Pitcher', 'Catcher', 'First baseman', 'Second baseman','Third baseman','Shortstop','Left fielder','Center fielder','Right fielder'],
+    'Basketball': ['','Point Guard', ,'Shooting Guard','Small Forward', 'Power Forward','Center'],
     'Bowling': ['N/A'],
-    'Cheerleading': ['Flyer', 'Base', 'Spotter','Tumbler','Backspot','Front spot'],
-    'Cross Country': ['Varsity Runner', 'Jr Varsity Runner', 'Pacer'],
-    'Dance Team': ['Choreographer', 'Section Leader'],
-    'Field Hockey': ['Goalkeeper','Defenders','Center-Back','Fullbacks','Midfielders','Center Midfielder','Wingers','Forwards','Strikers','Center Forward'],
-    'Football': ['Quarterback', 'Running Back',	'Wide Receiver'	,'Tight End','Offensive Lineman','Defensive Lineman','Linebacker','Cornerback',	'Safety','Kicker/Punter'],
+    'Cheerleading': ['','Flyer', 'Base', 'Spotter','Tumbler','Backspot','Front spot'],
+    'Cross Country': ['','Varsity Runner', 'Jr Varsity Runner', 'Pacer'],
+    'Dance Team': ['','Choreographer', 'Section Leader'],
+    'Field Hockey': ['','Goalkeeper','Defenders','Center-Back','Fullbacks','Midfielders','Center Midfielder','Wingers','Forwards','Strikers','Center Forward'],
+    'Football': ['','Quarterback', 'Running Back', 'Wide Receiver' ,'Tight End','Offensive Lineman','Defensive Lineman','Linebacker','Cornerback', 'Safety','Kicker/Punter'],
     'Golf': ['N/A'],
-    'Gymnastics': ['All-Around Gymnast', 'Artistic Gymnast', 'Apparatus Specialist', 'Floor Exercise Specialist' ,'Balance Beam Specialist','Uneven Bars Specialist', 'Vault Specialist', 'Rhythmic Gymnast', 'Trampoline Gymnast', 'Acrobatic Gymnast'],
-    'Hockey': ['Goaltender (Goalie)','Defensemen (Defenders)','Offensive Defenseman', 'Forwards', 'Center', 'Wingers' ],
-    'Lacross': ['Attack',	'Midfield',	'Defense', 	'Goalie'],
+    'Gymnastics': ['','All-Around Gymnast', 'Artistic Gymnast', 'Apparatus Specialist', 'Floor Exercise Specialist' ,'Balance Beam Specialist','Uneven Bars Specialist', 'Vault Specialist', 'Rhythmic Gymnast', 'Trampoline Gymnast', 'Acrobatic Gymnast'],
+    'Hockey': ['','Goaltender (Goalie)','Defensemen (Defenders)','Offensive Defenseman', 'Forwards', 'Center', 'Wingers' ],
+    'Lacross': ['','Attack', 'Midfield', 'Defense',  'Goalie'],
     'Skiing': ['N/A'],
-    'Soccer': ['Goalkeeper (GK)', 'Centerback','Fullback','Midfielder',	'Forwards'],
-    'Tennis': ['Singles','Doubles'],
-    'Swimming': ['Freestyle', 'Backstroke','Breaststroke', 'Butterfly',	'Individual Medley (IM)','Relay Events','Distance Specialists'],
-    'Track': ['Sprints' ,'Long-Distance', 'Hurdles', 'Relays', 'Jumps',	'Throws', 'Race Walk' ,'Multi-Events'],
-    'Volleyball': ['Setter' ,'Outside Hitter', 	'Middle Blocker', 	'Libero' ,'Defensive Specialist', 'Utility Player' ],
+    'Soccer': ['','Goalkeeper (GK)', 'Centerback','Fullback','Midfielder', 'Forwards'],
+    'Tennis': ['','Singles','Doubles'],
+    'Swimming': ['','Freestyle', 'Backstroke','Breaststroke', 'Butterfly', 'Individual Medley (IM)','Relay Events','Distance Specialists'],
+    'Track': ['','Sprints' ,'Long-Distance', 'Hurdles', 'Relays', 'Jumps', 'Throws', 'Race Walk' ,'Multi-Events'],
+    'Volleyball': ['','Setter' ,'Outside Hitter',  'Middle Blocker',  'Libero' ,'Defensive Specialist', 'Utility Player' ],
     'Weight Lifting': ['N/A'],
     'Wrestling': ['N/A']
    
-
-
-    
 };
 
 document.getElementById('sport').addEventListener('change', function() {
--- js/firstLetterCaptial.js
+++ js/firstLetterCaptial.js
@@ -1,12 +1,11 @@
-document.getElementById('career').addEventListener('input', function (event) {
+function capitalizeInput(event) {
     var value = event.target.value;
     event.target.value = value.replace(/\b[a-z]/g, function (match) {
         return match.toUpperCase();
     });
-});
-document.getElementById('fullName').addEventListener('input', function (event) {
-    var value = event.target.value;
-    event.target.value = value.replace(/\b[a-z]/g, function (match) {
-        return match.toUpperCase();
-    });
+}
+
+
+document.querySelectorAll('input.text-capitalize').forEach(function (input) {
+    input.addEventListener('input', capitalizeInput);
 });
